6a041ff4a7ba4803791a5d82478aff9b7b28cfa9
SpyMIner <vince051105@icloud.com>
2022-06-04 21:08:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from urllib import parse, request
 import re
 
 bot = discord.Client()
-bot = commands.Bot(command_prefix='?', description="This is a Helper Bot")
+bot = commands.Bot(command_prefix='.', description="This is a Helper Bot")
 bot.remove_command('help')
 
 @bot.command()
@@ -18,11 +18,7 @@ async def help(ctx):
     await ctx.send("To be added, " + ctx.message.author.mention)
    
 
-@bot.command()
-async def ip(ctx):
-    await ctx.send('Server:')
-    await ctx.send('Java: `play.animalparadise.tk`')
-    await ctx.send('Bedrock: `play.bedrock.animalparadise.tk` Port: `25566`')
+
     
 
 @bot.command()
@@ -37,15 +33,6 @@ async def info(ctx):
     
     await ctx.send(embed=embed)
 
-@bot.command()
-async def youtube(ctx, *, search):
-    query_string = parse.urlencode({'search_query': search})
-    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
-    # print(html_content.read().decode())
-    search_results = re.findall('href=\"\\/watch\\?v=(.{11})', html_content.read().decode())
-    # print(search_results)
-    # I will put just the first result, you can loop the response to show more results
-    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
 
 # Events
 @bot.event
@@ -57,12 +44,6 @@ async def on_ready():
 @bot.listen()
 async def on_message(message):
     return
-    #if " ip " in message.content.lower():
-    #    # in this case don't respond with the word "Tutorial" or you will call the on_message event recursively
-    #    await message.channel.send('Server:')
-    #    await message.channel.send('Java: `play.animalparadise.tk`')
-    #    await message.channel.send('Bedrock: `play.bedrock.animalparadise.tk` Port: `25566`')
-    #    await bot.process_commands(message)
 
     
-bot.run('OTgwNDY3MTI5NDExMzA1NDky.GA-RZr.rGjONbVcUL6Q6eJkcgDboRYWpl85AYC3DrrzEY')
+bot.run('OTQ0Nzc3OTM0OTE1Mzk5Njkx.GG7FnP.09BePYF6tn5AYPpkyk8mnHyoGtVxpGSJS-DiGQ')
